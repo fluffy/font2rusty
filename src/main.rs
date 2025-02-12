12bed73a14b68d6ddaec0184f84b81288bebc6f8
Cullen Jennings <fluffy@iii.ca>
2025-02-11 18:46:57 -0700

    start RLE encoding

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use fontdue::Font;
 #[allow(dead_code)]
 struct Glyph {
     rle_start: u16,
-    rle_len: u8,
     c: char,
     xmin: i8,
     ymin: i8,
@@ -11,7 +10,7 @@ struct Glyph {
     height: u8,
 }
 
-static _FONT_RLE_DATA: &'static [(i8, u8)] = &[(1, 255), (0, 0), (-10, 0), (3, 128)];
+static _FONT_RLE_DATA: &'static [(i8, u8)] = &[(0, 0), (0, 0), (-10, 0), (3, 128)];
 
 static _FONT_METRICS: &'static [Glyph] = &[
     Glyph {
@@ -21,7 +20,6 @@ static _FONT_METRICS: &'static [Glyph] = &[
         width: 10,
         height: 10,
         rle_start: 4,
-        rle_len: 3,
     },
     Glyph {
         c: 'b',
@@ -30,7 +28,6 @@ static _FONT_METRICS: &'static [Glyph] = &[
         width: 10,
         height: 10,
         rle_start: 4,
-        rle_len: 3,
     },
 ];
 
@@ -40,33 +37,56 @@ fn main() {
     // Load the font data from a file
     let font_data = include_bytes!("../fonts/NotoSans-Regular.ttf") as &[u8];
 
+    println!("// Do not edit - generated with font2rusty");
+    println!("// Font data for NotoSans-Regular.ttf");
+
+    println!("struct Glyph {{ rle_start: u16,");
+    println!("    c: char,xmin: i8,ymin: i8,width: u8,height: u8, }}");
+    println!("");
+
     // Parse the font
     let font = Font::from_bytes(font_data, fontdue::FontSettings::default()).unwrap();
 
-    for (c, index) in font.chars() {
+    println!("static FONT_RLE_DATA: &'static [(i8, u8)] = &[(0, 0),");
+    let mut rle_index = 1;
+
+    for (c, _index) in font.chars() {
         // Iterate over all glyphs
         if (*c > '~') || (*c < ' ') {
             continue;
         }
-        println!("Glyph {:?}: {:?}", index, c);
-    }
+        //println!("Glyph {:?}: {:?}", index, c);
 
-    // Use the font (e.g., to get the metrics of a character)
-    let (metrics, bitmap) = font.rasterize('g', 12.0);
-    println!("Character metrics: {:?}", metrics);
+        // Use the font (e.g., to get the metrics of a character)
+        let (metrics, bitmap) = font.rasterize(*c, 20.0);
+        //println!("Character metrics: {:?}", metrics);
 
-    // Print the bitmap
-    for y in 0..metrics.height {
-        for x in 0..metrics.width {
-            print!(
-                "{}",
-                if bitmap[y * metrics.width + x] > 50 {
-                    "#"
+        println!("// Char {} at {}", *c, rle_index);
+
+        // Run-length encode the bitmap
+        for y in 0..metrics.height {
+            let mut prev_val: u8 = 0;
+            let mut count: u8 = 0;
+
+            for x in 0..metrics.width {
+                let val = bitmap[y * metrics.width + x];
+                if val == prev_val {
+                    count += 1;
                 } else {
-                    " "
+                    print!("({}, {}),", count, 255-prev_val);
+                    rle_index += 1;
+
+                    prev_val = val;
+                    count = 1;
                 }
-            );
+            }
+
+            println!("(-1, 255),");
+            rle_index += 1;
         }
-        println!();
+        println!("(0, 0),");
+        rle_index += 1;
     }
+
+    println!("];"); // end of FONT_RLE_DATA
 }
