7ebbc977b0075c40f1977135e5901f9ef28baff6
Cullen Jennings <fluffy@iii.ca>
2025-02-11 18:06:42 -0700

    start output file structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 use fontdue::Font;
 
+#[allow(dead_code)]
+struct Glyph {
+    rle_start: u16,
+    rle_len: u8,
+    c: char,
+    xmin: i8,
+    ymin: i8,
+    width: u8,
+    height: u8,
+}
+
+static _FONT_RLE_DATA: &'static [(i8, u8)] = &[(1, 255), (0, 0), (-10, 0), (3, 128)];
+
+static _FONT_METRICS: &'static [Glyph] = &[
+    Glyph {
+        c: 'a',
+        xmin: 0,
+        ymin: 0,
+        width: 10,
+        height: 10,
+        rle_start: 4,
+        rle_len: 3,
+    },
+    Glyph {
+        c: 'b',
+        xmin: 0,
+        ymin: 0,
+        width: 10,
+        height: 10,
+        rle_start: 4,
+        rle_len: 3,
+    },
+];
+
+static _FONT_GLYPH_INDEX: &'static [u8] = &[0, 0, 0, 23, 22];
 
 fn main() {
     // Load the font data from a file
@@ -8,13 +43,13 @@ fn main() {
     // Parse the font
     let font = Font::from_bytes(font_data, fontdue::FontSettings::default()).unwrap();
 
-    for (c,index) in font.chars() { // Iterate over all glyphs
-       if (*c > '~') || ( *c < ' ') {
-           continue;
-       }
-        println!("Glyph {:?}: {:?}", index, c );
+    for (c, index) in font.chars() {
+        // Iterate over all glyphs
+        if (*c > '~') || (*c < ' ') {
+            continue;
+        }
+        println!("Glyph {:?}: {:?}", index, c);
     }
-
 
     // Use the font (e.g., to get the metrics of a character)
     let (metrics, bitmap) = font.rasterize('g', 12.0);
@@ -23,7 +58,14 @@ fn main() {
     // Print the bitmap
     for y in 0..metrics.height {
         for x in 0..metrics.width {
-            print!("{}", if bitmap[y * metrics.width + x] > 50 { "#" } else { " " });
+            print!(
+                "{}",
+                if bitmap[y * metrics.width + x] > 50 {
+                    "#"
+                } else {
+                    " "
+                }
+            );
         }
         println!();
     }
